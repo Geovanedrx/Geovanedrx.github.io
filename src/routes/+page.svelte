b18ee3aa843f94ff3db92e83438892c749bca92b
Geovanedrx <geoedu345@gmail.com>
2025-01-22 14:37:22 -0400

    asd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-este site é um grande teste
+<h2>este site é um grande teste</h2>
