aedd4ca9f247e3cf34fad599dcd79df503ae9a21
Geovanedrx <geoedu345@gmail.com>
2025-01-28 15:15:26 -0400

    testando 3.0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
+<p> alguma coisa</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation para fazer um teste de novo</p>
   <style>
     .hero {
